6af81953e5b8be7f05af34b2130d4d9ce64cb1c7
samippun <pun.samip04@gmail.com>
2026-01-06 23:08:50 +0545

    setup for fyp

---
+++ src/main/java/com/strange/StrangeApplication.java
@@ -0,0 +1,13 @@
+package com.strange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strange/StrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
